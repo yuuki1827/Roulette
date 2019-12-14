93e2066789b7934c95af48956d10661e58e3e6e4
yuuki1827 <kouaka.hibatsuna@gmail.com>
2019-12-15 00:45:20 +0900

    ルーレット回転から減速停止まで

--- Assets/RouletteController.cs
+++ Assets/RouletteController.cs
@@ -21,7 +21,21 @@ public class RouletteController : MonoBehaviour
             this.rotSpeed = 10;
         }
 
+        /*
+         * GetMouseButtonDown = マウスがクリックされた瞬間に「TRUE」を返す
+         * GetMouseButtonUp = マウスボタンが離された瞬間に「TRUE」を返す
+         * GetMouseButton = マウスがクリックされている間は「TRUE」を返す
+         */
+
         // 回転速度分、ルーレットを回転させる
         transform.Rotate(0, 0, this.rotSpeed);
+
+        /*
+         * Rotate = 回転メソッド
+         * (X軸回転, Y軸回転, Z軸回転)
+         */
+
+        // ルーレットを減速させる
+        this.rotSpeed *= 0.96f;
     }
 }
