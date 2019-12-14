bb1332359c2b286d5146bdd39dde5292d64058d1
yuuki1827 <kouaka.hibatsuna@gmail.com>
2019-12-14 21:26:07 +0900

    Unityの教科書 Chapter3 ルーレットアプリ作成

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    // 回転速度
+    float rotSpeed = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マウスが押されたら回転速度を設定する
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        // 回転速度分、ルーレットを回転させる
+        transform.Rotate(0, 0, this.rotSpeed);
+    }
+}
